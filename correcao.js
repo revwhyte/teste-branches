9fc254d603bd397087ba49b7b756fab44cd7d8c8
Philippe Correia <revwhyte@gmail.com>
2020-09-16 11:51:45 -0300

    atendendo demanda card#1000

---
+++ correcao.js
@@ -0,0 +1,6 @@
+// demanda atendida
+
+var soma = ((a, b) => { return a + b; });
+var subtracao = ((a, b) => { return a - b; });
+var produto = ((a, b) => { return a * b; });
+var divisao = ((a, b) => { return b ?  a / b : '!div(0)'; });
